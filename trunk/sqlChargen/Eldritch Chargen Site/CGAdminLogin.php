ac15ae0292b98f96ce132149ae0d9d2df63b57c4
allen.anselmo <allen.anselmo@0a6c793f-da54-0410-8529-15adaaf89f9c>
2009-02-08 03:14:16 +0000

    modified admin DB and login code

---
+++ trunk/sqlChargen/Eldritch Chargen Site/CGAdminLogin.php
@@ -0,0 +1,31 @@
+<?
+	error_reporting(E_ALL);
+	header("Cache-Control: no-cache");
+
+	include 'CGAdminDB.php';
+	//need to create this file. it's not part of the SVN. It needs to have $password_eldritchSQL and $AES_ENC_KEY
+	include 'CGAdminPasswords.php';
+	
+	$loginUsername = isset($_POST["loginUsername"]) ? $_POST["loginUsername"] : "";
+	$loginPassword = isset($_POST["loginPassword"]) ? $_POST["loginPassword"] : "";
+	 
+	$eldritchSQL = mysql_pconnect($hostname_eldritchSQL, $username_eldritchSQL, $password_eldritchSQL) 
+					or trigger_error(mysql_error(),E_USER_ERROR); 
+	mysql_select_db($database_eldritchSQL) 
+		or trigger_error(mysql_error(),E_USER_ERROR);
+		
+	//"INSERT INTO CGDB_ADMIN VALUES ('temp',AES_ENCRYPT('temp','Bella_My_Love') )"
+		
+	$result = mysql_query("SELECT * FROM CGDB_ADMIN WHERE User='".$loginUsername."' AND Pass=AES_ENCRYPT('".$loginPassword."','".$AES_ENC_KEY."')")
+		or trigger_error("Error selecting administrators" . mysql_error(), E_USER_ERROR);
+	
+	if (mysql_num_rows($result) == 1)
+	{
+		echo("{success: true}");
+	}
+	else 
+	{
+		echo "{success: false, errors: { reason: 'Login failed. Try again.' }}";
+		break;
+	}	
+?>
